a86d06fd7e1a9c306ec48f2093f5199316f4e781
WarWolfen <warwolfen@gmail.com>
2026-03-12 12:51:46 +0100

    New logging

--- logger.js
+++ logger.js
@@ -8,17 +8,10 @@ const logger = pino({
 			{
 				target: 'pino-pretty',
 				options: { colorize: true, }
-				level: 'info',
 			},
 			{
 				target: 'pino-roll',
 				options: { file: './public/log/combined-%DATE%.txt', mkdir: true, size: '10m', limit: { count: 5 } }
-				level: 'info',
-			},
-			{
-				target: 'pino-roll',
-				options: { file: './public/log/error-%DATE%.txt', mkdir: true, size: '10m', limit: { count: 5 } }
-				level: 'info',
 			},
 		]
 	}
