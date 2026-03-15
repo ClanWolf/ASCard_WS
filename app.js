0cdd687657d233c322dd523d9062c9a07c612e1f
WarWolfen <warwolfen@gmail.com>
2026-03-15 13:07:32 +0100

    Name changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const expHbs = require("express-handlebars");
 
 let date = new Date().toISOString().split("T")[0];
 
-logger.info("Starting up cw-g_ws (Webservice)...");
+logger.info("Starting up CW_DB-WS (Database Webservice)...");
 
 var handlebars = expHbs.create({
   defaultLayout: "main-layout",
@@ -54,7 +54,7 @@ app.use("/games", require("./routes/games"));
 
 app.use("/", (req, res) => {
   res.render("home.handlebars", {
-    pageTitle: "cw-g_ws (Webservice)",
+    pageTitle: "CW_DB-WS (Database Webservice)",
     date: date,
     activeHome: true,
   });
