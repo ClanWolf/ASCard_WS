792fd86a2befded64ed093b53e64ade46c1b4e16
Christian Bartel <warwolfen@gmail.com>
2026-04-17 14:56:56 +0200

    Disable pino-toke logger configuration
    Comment out unused pino-toke logger configuration

--- logger.js
+++ logger.js
@@ -13,6 +13,17 @@ const logger = pino({
 				target: 'pino-roll',
 				options: { file: './public/log/combined.txt', mkdir: true, size: '10m', limit: { count: 5 } }
 			},
+			// https://github.com/pinojs/pino-toke
+//			{
+//				target: 'pino-toke',
+//				level: 'info',
+//				options: {
+//					destination: 1, // optional (default stdout)
+//					ancellary: 2, // optional
+//					format: ':method :url :status :res[content-length] - :response-time ms', // required
+//					keep: false // optional
+//				}
+//			}
 		]
 	}
 });
